99cbf644631d3fd2192c2c2ac9c2f1ff3d40d2e7
R3GG3 <konrad.soczi@gmail.com>
2021-08-24 11:46:16 +0200

    Edited README, changed req

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
 from distutils.core import setup
 setup(
-  name = 'menu',         # How you named your package folder (MyLib)
-  packages = ['menu'],   # Chose the same as "name"
+  name = 'SimpleR0cKyMenu',         # How you named your package folder (MyLib)
+  packages = ['SimpleR0cKyMenu'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Creating Simple Menu for your program',   # Give a short description about your library
@@ -12,7 +12,6 @@ setup(
   download_url = 'https://github.com/R3GG3/menu/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
   keywords = ['MENU', 'SIMPLE', 'PYTHON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'os',
           'pynput',
       ],
   classifiers=[
@@ -20,6 +19,5 @@ setup(
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to suppor
   ],
 )
